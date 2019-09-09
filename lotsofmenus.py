8c29f707da1f579c3dfc4fcef683cc88bd7bafff
ShiminLei <sl2kd@virginia.edu>
2019-09-09 13:10:34 -0400

    new

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -71,7 +71,7 @@ session = DBSession()
 # Create dummy user
 # User1 = User(name="Robo Barista", email="tinnyTim@udacity.com",
 #              picture='https://pbs.twimg.com/profile_images/2671170543/18debd694829ed78203a5a36dd364160_400x400.png')
-User1 = User(name="Shimin Lei", email="sl2kd@virginia.edu",
+User1 = User(name="Shimin Lei", email="503622593@qq.com",
              picture='https://i.screenshot.net/j32wxtq')
 session.add(User1)
 session.commit()
