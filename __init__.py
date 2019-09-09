e403774639825c633e42909e70e7f387b44827ed
ShiminLei <sl2kd@virginia.edu>
2019-09-09 03:06:08 -0400

    test lines removed

--- __init__.py
+++ __init__.py
@@ -118,7 +118,7 @@ def gconnect():
     access_token = credentials.access_token
     url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
            % access_token)
-    h = httplib2.Http(disable_ssl_certificate_validation=True)
+    h = httplib2.Http()
     result = json.loads(h.request(url, 'GET')[1])
     # result_string = demjson.encode(result)
     # print('############')
